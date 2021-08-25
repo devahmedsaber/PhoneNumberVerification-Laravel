431066ec0c76de81314c5045a92697321d70c56f
Ahmed Saber <devahmedsaber@gmail.com>
2021-08-25 16:01:15 +0200

    Edit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,22 +13,22 @@
 
     <!--[if lt IE 10]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.{{url('/')}}/public/assets/js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description"
-          content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
-    <meta name="keywords"
-          content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
-    <meta name="author" content="colorlib" />
 
     <link rel="icon" href="https://colorlib.com/polygon/admindek/files/assets/images/favicon.ico" type="image/x-icon">
 
-    <link rel="stylesheet" type="text/css" href="{{url('/')}}/semantic/semantic.min.css">
-    <script src="{{url('/')}}/semantic/semantic.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="{{url('/')}}/public/assets/semantic/semantic.min.css">
+
+    <script src="{{url('/')}}/public/assets/semantic/semantic.min.js"></script>
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
@@ -529,50 +529,6 @@
     </div>
 </div>
 
-
-<!--[if lt IE 10]>
-<div class="ie-warning">
-<h1>Warning!!</h1>
-<p>You are using an outdated version of Internet Explorer, please upgrade
-    <br/>to any of the following web browsers to access this website.
-</p>
-<div class="iew-container">
-    <ul class="iew-download">
-        <li>
-            <a href="http://www.google.com/chrome/">
-                <img src="../files/assets/images/browser/chrome.png" alt="Chrome">
-                <div>Chrome</div>
-            </a>
-        </li>
-        <li>
-            <a href="https://www.mozilla.org/en-US/firefox/new/">
-                <img src="../files/assets/images/browser/firefox.png" alt="Firefox">
-                <div>Firefox</div>
-            </a>
-        </li>
-        <li>
-            <a href="http://www.opera.com">
-                <img src="../files/assets/images/browser/opera.png" alt="Opera">
-                <div>Opera</div>
-            </a>
-        </li>
-        <li>
-            <a href="https://www.apple.com/safari/">
-                <img src="../files/assets/images/browser/safari.png" alt="Safari">
-                <div>Safari</div>
-            </a>
-        </li>
-        <li>
-            <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
-                <img src="../files/assets/images/browser/ie.png" alt="">
-                <div>IE (9 & above)</div>
-            </a>
-        </li>
-    </ul>
-</div>
-<p>Sorry for the inconvenience!</p>
-</div>
-<![endif]-->
 
 
 <script data-cfasync="false" src="{{url('/')}}/public/assets/js/email-decode.min.js"></script>
